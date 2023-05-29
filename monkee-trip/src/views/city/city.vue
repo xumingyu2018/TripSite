12a8f79eeb2c3f21269c2afc245cc3caad5fdc9c
Nevermore <xmy981022@163.com>
2023-05-29 16:53:22 +0800

    实现位置数据tab切换展示

--- monkee-trip/src/views/city/city.vue
+++ monkee-trip/src/views/city/city.vue
@@ -1,29 +1,38 @@
 <template>
   <div class="city top-page">
-    <van-search
-      v-model="searchValue"
-      shape="round"
-      placeholder="城市/区域/位置" 
-      show-action
-      @cancel="onCancel"
-    />
+    <div class="top">
+      <!-- 1.搜索框 -->
+      <van-search
+        v-model="searchValue"
+        shape="round"
+        placeholder="城市/区域/位置" 
+        show-action
+        @cancel="onCancel"
+      />
+      <!-- 2.tab的切换 -->
+      <!-- tabActive默认索引,加上name属性即可作为标签的索引值 -->      
+      <van-tabs v-model:active="tabActive" color="#fff9854">
+        <!-- <van-tab title="国内港澳台"></van-tab> -->
+        <!-- <van-tab :title="allCity?.cityGroupOverSea?.title"></van-tab> -->
+        <template v-for="(value, key, index) in allCities" :key="key">
+          <van-tab :title="value.title" :name="key"></van-tab>
+        </template>
+      </van-tabs>
+    </div>
 
-  <van-tabs v-model:active="tabActive" color="#fff9854">
-    <!-- <van-tab title="国内港澳台"></van-tab> -->
-    <!-- <van-tab :title="allCity?.cityGroupOverSea?.title"></van-tab> -->
-    <template v-for="(value, key, index) in allCities" :key="key">
-      <van-tab :title="value.title"></van-tab>
-    </template>
-  </van-tabs>
-
+    <div class="content">
+      <!-- group-data父传子 -->
+      <city-group :group-data="currentGroup"/>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import { storeToRefs } from "pinia";
 import { useRouter } from "vue-router";
 import useCityStore from "@/stores/modules/city";
-import { storeToRefs } from "pinia";
+import CityGroup from "./components/city-group.vue";
 // import axios from "axios";
 // 从默认的index.js中导入
 // import { getCityAll } from "@/services";
@@ -60,8 +69,30 @@ const cityStore = useCityStore()
 cityStore.fetchAllCitiesData()
 const { allCities } = storeToRefs(cityStore)
 
+// 目的: 获取选中标签后的数据
+// 1.获取正确的key: 将tabs中绑定的tabAction正确绑定
+// 2.根据key从allCities获取数据, 默认直接获取的数据不是响应式的, 所以必须包裹computed
+const currentGroup = computed(() => allCities.value[tabActive.value])
+
 </script>
 
 <style lang="less" scoped>
-
+.city {
+  // 使用top固定定位实现
+  // .top {
+  //   position: fixed;
+  //   top: 0;
+  //   left: 0;
+  //   right: 0;
+  // }
+  // .content {
+  //   margin-top: 98px;
+  // }
+  
+  // 局部滚动
+  .content {
+    height: calc(100vh - 98px);
+    overflow: auto;
+  }
+}
 </style>
---
+++ monkee-trip/src/views/city/components/city-group.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="city-group">
+    <template v-for="(group, index) in groupData?.cities" :key="index">
+      <div class="group-item">
+        <h2 class="title">分组：{{ group.group }}</h2>
+        <div class="list">
+          <template v-for="(city, indey) in group.cities" :key="indey">
+            <div class="city">{{ city.cityName }}</div>
+          </template>
+        </div>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  groupData: {
+    type: Object,
+    default: () => ({})
+  }
+})
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
