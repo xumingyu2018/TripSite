32d8ad1b8e962099bec8ffcd4acbb56ac308647e
Nevermore <xmy981022@163.com>
2023-05-29 15:23:17 +0800

    指定页面隐藏底部导航栏功能

--- monkee-trip/src/App.vue
+++ monkee-trip/src/App.vue
@@ -3,13 +3,16 @@
     <router-view/>
 
     <tab-bar></tab-bar>
+    <!-- 方法一：隐藏底部导航栏 -->
+    <!-- <tab-bar v-if="!route.meta.hideTabBar"></tab-bar> -->
   </div>
 </template>
 
 <script setup>
 import TabBar from '@/components/tab-bar/tabbar.vue'
+// import { useRoute } from 'vue-router'
 
-
+// const route = useRoute()
 </script>
 
 <style scoped>
