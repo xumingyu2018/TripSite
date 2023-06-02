9def03ff8cbf4dc4fc31040515c541f92f01c5e2
Nevermore <xmy981022@163.com>
2023-06-02 20:23:09 +0800

    使用throttle节流处理滚动事件

--- monkee-trip/src/hooks/useScroll.js
+++ monkee-trip/src/hooks/useScroll.js
@@ -1,4 +1,5 @@
 import { onMounted, onUnmounted, ref } from 'vue';
+import { throttle } from 'underscore'
 
 export default function useScroll(){
   const isReachBottom = ref(false)
@@ -6,16 +7,16 @@ export default function useScroll(){
   const scrollTop = ref(0)
   const scrollHeight = ref(0)
 
-  const scrollListenerHandle = () => {
+  const scrollListenerHandle = throttle(() => {
     clientHeight.value = document.documentElement.clientHeight
     scrollTop.value = document.documentElement.scrollTop
     scrollHeight.value = document.documentElement.scrollHeight
-
+    console.log("监听到滚动");
     if(clientHeight.value + scrollTop.value >= scrollHeight.value) {
       console.log("滚动到底部了");
       isReachBottom.value = true
     }
-  }
+  }, 100)
   
   onMounted(() => {
     window.addEventListener("scroll", scrollListenerHandle, false)
