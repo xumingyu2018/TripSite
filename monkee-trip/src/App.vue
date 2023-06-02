69151b9a99dc5bc0d70c949d8f9fc7fe921c2eaa
Nevermore <xmy981022@163.com>
2023-06-02 21:23:43 +0800

    搭建loading页面组件

--- monkee-trip/src/App.vue
+++ monkee-trip/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="app">
     <router-view/>
-
     <!-- <tab-bar/> -->
     <!-- 方法一：隐藏底部导航栏 -->
     <tab-bar v-if="!route.meta.hideTabBar"></tab-bar>
+    <loading/>
+
   </div>
 </template>
 
 <script setup>
 import TabBar from '@/components/tab-bar/tabbar.vue'
 import { useRoute } from 'vue-router'
+import loading from '@/components/loading/loading.vue'
+
 
 const route = useRoute()
 </script>
