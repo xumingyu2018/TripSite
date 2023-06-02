d571bc41d9cda3aada63be245820fe4ebb8e441d
Nevermore <xmy981022@163.com>
2023-06-02 20:00:53 +0800

    优化hooks滚动事件

--- monkee-trip/src/hooks/useScroll.js
+++ monkee-trip/src/hooks/useScroll.js
@@ -1,35 +1,17 @@
-import { onDeactivated, onMounted, onUnmounted, ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
 
-
-// export default function useScroll(reachBottomCB) {
-//   const scrollListenerHandler = () => {
-//     const clientHeight = document.documentElement.clientHeight
-//     const scrollTop = document.documentElement.scrollTop
-//     const scrollHeight = document.documentElement.scrollHeight
-//     console.log("-------")
-//     if (clientHeight + scrollTop >= scrollHeight) {
-//       console.log("滚动到底部了")
-//       if (reachBottomCB) reachBottomCB()
-//     }
-//   }
-  
-//   onMounted(() => {
-//     window.addEventListener("scroll", scrollListenerHandler)
-//   })
-  
-//   onUnmounted(() => {
-//     window.removeEventListener("scroll", scrollListenerHandler)
-//   })
-// }
 export default function useScroll(){
   const isReachBottom = ref(false)
+  const clientHeight = ref(0)
+  const scrollTop = ref(0)
+  const scrollHeight = ref(0)
 
   const scrollListenerHandle = () => {
-    const clientHeight = document.documentElement.clientHeight
-    const scrollTop = document.documentElement.scrollTop
-    const scrollHeight = document.documentElement.scrollHeight
+    clientHeight.value = document.documentElement.clientHeight
+    scrollTop.value = document.documentElement.scrollTop
+    scrollHeight.value = document.documentElement.scrollHeight
 
-    if(clientHeight + scrollTop >= scrollHeight) {
+    if(clientHeight.value + scrollTop.value >= scrollHeight.value) {
       console.log("滚动到底部了");
       isReachBottom.value = true
     }
@@ -43,5 +25,5 @@ export default function useScroll(){
     window.removeEventListener("scroll", scrollListenerHandle, false)
   })
 
-  return { isReachBottom }
+  return { isReachBottom, clientHeight, scrollTop, scrollHeight }
 }
