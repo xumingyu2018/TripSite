d2a351d73f58e4a71c06e639e458a007e8b6c5bc
Nevermore <xmy981022@163.com>
2023-06-02 20:39:24 +0800

    Tabbar的默认索引bug处理

--- monkee-trip/src/components/tab-bar/tabbar.vue
+++ monkee-trip/src/components/tab-bar/tabbar.vue
@@ -1,7 +1,7 @@
 <!-- 自定义底部导航栏组件 -->
 <template>
   <div class="tab-bar">
-    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+    <van-tabbar v-model="currentIndex" active-color="#ff9854" route>
       <template v-for="(item,index) in tabbarData">
         <van-tabbar-item :to="item.path">
           <template #default>
@@ -22,10 +22,18 @@
 
 import tabbarData from '@/assets/data/tabbar.js'
 import { getAssetURL } from '@/utils/load_assets.js';
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
 
+// 监听路由改变时，找到对应路由的索引，设置currentIndex
 const currentIndex = ref(0);
+const route = useRoute();
+watch(route, (newRoute) => {
+  const index = tabbarData.findIndex((item) => item.path === newRoute.path);
+  if(index === -1) return;
+  currentIndex.value = index;
+});
+
 
 </script>
 
