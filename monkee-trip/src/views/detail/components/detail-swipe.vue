5f9967b26ee328a44578eb76241b6a4c5d97eff8
Nevermore <xmy981022@163.com>
2023-06-03 21:41:54 +0800

    自定义轮播指示器

--- monkee-trip/src/views/detail/components/detail-swipe.vue
+++ monkee-trip/src/views/detail/components/detail-swipe.vue
@@ -6,6 +6,21 @@
           <img :src="item.url" alt="">
         </van-swipe-item>
       </template>
+      
+      <!-- 默认是圆点，这里是作用域插槽和具名插槽,对props解构（active是选中的指示器） -->
+      <template #indicator="{ active, total }">
+        <div class="indicator">
+          <template v-for="(value, key, index) in swipeGroup" :key="key">
+            <!-- 选中的一组动态绑定class样式，显示数字(这里key是字符串类型) -->
+            <span class="item" :class="{ active: swipeData[active]?.enumPictureCategory == key }">
+              <span class="text">{{ getName(value[0].title) }}</span>
+              <span class="count" v-if="swipeData[active]?.enumPictureCategory == key">
+                {{ getCategoryIndex(swipeData[active]) }}/{{ value.length }}
+              </span>
+            </span>
+          </template>
+        </div>
+      </template>
     </van-swipe>
   </div>
 </template>
@@ -19,6 +34,51 @@ const props = defineProps({
   }
 })
 
+// 对数据进行转换（自定义指示器）
+const swipeGroup = {}
+
+// 数据处理样式
+// {
+//   "2": [item1, item2, item3 ...],
+//   "4": [itemx, itemy, itemz ...],
+//   "9": [itema, itemb, itemc ...]
+// }
+// 思路一: 好理解, 两次循环
+// for (const item of props.swipeData) {
+//   swipeGroup[item.enumPictureCategory] = []
+// }
+// for (const item of props.swipeData) {
+//   const valueArray = swipeGroup[item.enumPictureCategory]
+//   valueArray.push(item)
+// }
+// 思路二: 一次循环
+for (const item of props.swipeData) {
+  let valueArray = swipeGroup[item.enumPictureCategory]
+  if (!valueArray) {
+    valueArray = []
+    swipeGroup[item.enumPictureCategory] = valueArray
+  }
+  valueArray.push(item)
+}
+
+// 定义转换数据的方法(非贪婪匹配)
+// （【卧室】：【卫生间】：【其他】：）去掉冒号
+const nameReg = /【(.*?)】/i
+const getName = (name) => {
+  // return name.replace("：", "").replace("】", "").replace("【", "")
+  const results = nameReg.exec(name)
+  return results[1]
+}
+
+// 处理在当前类别中的数组索引（如"2"中的item2索引为2）
+const getCategoryIndex = (item) => {
+  // 获取当前类别的数组
+  const valueArray = swipeGroup[item.enumPictureCategory]
+  // 使用findIndex查出当前item在数组中的索引
+  return valueArray.findIndex(data => data === item) + 1
+}
+
+
 </script>
 
 <style lang="less" scoped>
@@ -30,6 +90,27 @@ const props = defineProps({
       }
     }
 
+    .indicator {
+      position: absolute;
+      right: 5px;
+      bottom: 5px;
+      display: flex;
+      padding: 2px 5px;
+      font-size: 12px;
+      color: #fff;
+      background: rgba(0, 0, 0, 0.8);
+
+      .item {
+        margin: 0 3px;
+
+        &.active {
+          padding: 0 3px;
+          border-radius: 5px;
+          background-color: #fff;
+          color: #333;
+        }
+      }
+    }
   }
 }
 </style>
