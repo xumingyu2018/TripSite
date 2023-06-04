446b02a24b7fed22cfa99521b161e9a9d2c64671
Nevermore <xmy981022@163.com>
2023-06-04 21:08:38 +0800

    Tab-control标签页的滚动效果初步实现

--- monkee-trip/src/views/detail/detail.vue
+++ monkee-trip/src/views/detail/detail.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="detail top-page">
+  <div class="detail top-page" ref="detailRef">
+    <tab-control v-if="showTabControl" :titles="['aaa', 'bbb', 'ccc', 'ddd' ]" class="tabs"/>
     <van-nav-bar title="房屋详情" left-text="旅途" left-arrow  @click-left="onClickLeft"/>
     <div class="main" v-if="mainPart">
       <detail-swipe :swipe-data="mainPart.topModule.housePicture.housePics"/>
@@ -28,10 +29,12 @@ import DetailComment from "./components/detail_05-comment.vue";
 import DetailNotice from "./components/detail_06-notice.vue";
 import DetailMap from "./components/detail_07-map.vue";
 import DetailIntro from "./components/detail_08-intro.vue";
+import TabControl from "@/components/tab-control/tab-control.vue";
 
 import { getDetailInfos } from '@/services';
 import { useRoute, useRouter } from 'vue-router';
 import { computed, ref } from "vue";
+import useScroll from "@/hooks/useScroll";
 
 const router = useRouter()
 const route = useRoute()
@@ -48,9 +51,25 @@ getDetailInfos(houseId).then(res => {
 const onClickLeft = () => {
   router.back()
 }
+
+// tabControll标签页的相关操作
+const detailRef = ref()
+// 传入参数，监听Dom元素的滚动事件，而不是window
+const { scrollTop } = useScroll(detailRef)
+const showTabControl = computed(() => {
+  return scrollTop.value >= 300
+})
+
 </script>
 
 <style lang="less" scoped>
+.tabs {
+  position: fixed;
+  z-index: 9;
+  left: 0;
+  right: 0;
+  top: 0;
+}
 
 .footer {
   display: flex;
