6561948db89248d2145cd7bcd7ff607d80e1203f
Nevermore <xmy981022@163.com>
2023-06-04 20:34:54 +0800

    （优化）窗口滚动同时监听window和Dom元素

--- monkee-trip/src/hooks/useScroll.js
+++ monkee-trip/src/hooks/useScroll.js
@@ -1,17 +1,28 @@
 import { onMounted, onUnmounted, ref } from 'vue';
 import { throttle } from 'underscore'
 
-export default function useScroll(){
+export default function useScroll(elRef){
+  // 默认不传参监听的是window的窗口滚动
+  let el = window
+
   const isReachBottom = ref(false)
   const clientHeight = ref(0)
   const scrollTop = ref(0)
   const scrollHeight = ref(0)
 
   const scrollListenerHandle = throttle(() => {
-    clientHeight.value = document.documentElement.clientHeight
-    scrollTop.value = document.documentElement.scrollTop
-    scrollHeight.value = document.documentElement.scrollHeight
-    console.log("监听到滚动");
+    if(el === window){
+      // window窗口的滚动
+      clientHeight.value = document.documentElement.clientHeight
+      scrollTop.value = document.documentElement.scrollTop
+      scrollHeight.value = document.documentElement.scrollHeight
+    } else {
+      // Dom元素上的滚动（加了over-flow）
+      clientHeight.value = el.clientHeight
+      scrollTop.value = el.scrollTop
+      scrollHeight.value = el.scrollHeight
+    }
+
     if(clientHeight.value + scrollTop.value >= scrollHeight.value) {
       console.log("滚动到底部了");
       isReachBottom.value = true
@@ -19,11 +30,12 @@ export default function useScroll(){
   }, 100)
   
   onMounted(() => {
-    window.addEventListener("scroll", scrollListenerHandle, false)
+    if(elRef) el = elRef.value
+    el.addEventListener("scroll", scrollListenerHandle, false)
   })
   
   onUnmounted(() => {
-    window.removeEventListener("scroll", scrollListenerHandle, false)
+    el.removeEventListener("scroll", scrollListenerHandle, false)
   })
 
   return { isReachBottom, clientHeight, scrollTop, scrollHeight }
